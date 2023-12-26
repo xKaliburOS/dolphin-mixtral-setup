50cf6341da1e8e57347a9187dd51fa2b4f15d979
Deyao Chen <chendeyao000@gmail.com>
2023-12-26 18:08:02 +0800

    update

--- install-ui.sh
+++ install-ui.sh
@@ -2,6 +2,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 20
+nvm use 20
 git clone https://github.com/ollama-webui/ollama-webui.git
 cd ollama-webui/
 
