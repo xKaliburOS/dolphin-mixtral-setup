877eba91e605570b3edc81598973f10c5b925a43
Deyao Chen <chendeyao000@gmail.com>
2023-12-26 18:49:06 +0800

    update server logic

--- create-model.sh
+++ create-model.sh
@@ -1,7 +1,12 @@
 ollama serve > /dev/null &
+PID=$!
+trap ctrl_c INT
+
+function ctrl_c() {
+    kill -9 $PID
+}
 until ollama create badbad -f ./Modelfile
 do
 sleep 1
 done
-PID=$!
 kill -9 $PID
--- start-ui.sh
+++ start-ui.sh
@@ -1,3 +1,10 @@
+ollama serve > /dev/null &
+PID=$!
+trap ctrl_c INT
+
+function ctrl_c() {
+    kill -9 $PID
+}
 cd ollama-webui/backend
 if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
     . "$HOME/miniconda3/etc/profile.d/conda.sh"
@@ -8,3 +15,5 @@ fi
 conda activate
 
 uvicorn main:app --host 0.0.0.0 --port 8081
+
+kill -9 $PID
